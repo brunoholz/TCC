877d693692c29d54acb015fbbcb8680f680ecc54
xiaohuolong07 <natan_will88@hotmail.com>
2022-11-24 09:34:47 -0300

    comitado

--- TCC/JS/main.js
+++ TCC/JS/main.js
@@ -1,1 +1,9 @@
+/* Voltar a página para cima na imagem da empresa. */
 
+var voltarParaCima = document.querySelector('#home');
+
+voltarParaCima.addEventListener('click', function () {
+    window.scrollTo(0, 0);
+});
+
+// ---------------------
