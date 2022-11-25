de18da059ab5532541076b744cf1bbc8dbb42fc0
xiaohuolong07 <natan_will88@hotmail.com>
2022-11-25 11:05:56 -0300

    Atualização

--- TCC/JS/main.js
+++ TCC/JS/main.js
@@ -1,9 +1,39 @@
 /* Voltar a página para cima na imagem da empresa. */
 
-var voltarParaCima = document.querySelector('#home');
+let voltarParaCima = document.querySelector('#home');
 
 voltarParaCima.addEventListener('click', function () {
     window.scrollTo(0, 0);
 });
 
 // ---------------------
+
+// Function para abrir o insta no Card
+
+function abrirInsta() {
+    let brunoInsta = document.querySelector('.card-1');
+
+    brunoInsta.addEventListener('click', function () {
+        window.open('https://www.instagram.com/holzbruno/');
+    });
+
+    let nataInsta = document.querySelector('.card-2');
+
+    nataInsta.addEventListener('click', function () {
+        window.open('https://www.instagram.com/xiaohuolong0788/');
+    });
+
+    let gabrielInsta = document.querySelector('.card-3');
+
+    gabrielInsta.addEventListener('click', function () {
+        window.open('https://www.instagram.com/nipponic_ofc/');
+    });
+
+    let lucasInsta = document.querySelector('.card-4');
+
+    lucasInsta.addEventListener('click', function () {
+        window.open('https://www.instagram.com/llukasoliveira/');
+    });
+}
+
+abrirInsta();
